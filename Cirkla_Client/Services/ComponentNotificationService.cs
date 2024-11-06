e663595319e1ddb64343876538a84e7e70f73fd6
Samsal <43518070+baclava1001@users.noreply.github.com>
2024-11-06 17:01:50 +0100

    -Inbox components now rerender together. -Added notificationService for sibling components.

---
+++ Cirkla_Client/Services/ComponentNotificationService.cs
@@ -0,0 +1,11 @@
+﻿namespace Cirkla_Client.Services
+{
+    public class ComponentNotificationService
+    {            
+        // Action to notify components of updates
+        public Action OnNotify { get; set; }
+
+        // Method to trigger the notification
+        public void NotifyStateChanged() => OnNotify?.Invoke();
+    }
+}
