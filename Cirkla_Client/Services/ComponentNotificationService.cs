90869f61bab7021bc9f5eb5e66395b49076633bb
Samsal <43518070+baclava1001@users.noreply.github.com>
2024-11-07 13:53:43 +0100

    InboxIncoming correctly triggers rerender in InboxHistory.

--- Cirkla_Client/Services/ComponentNotificationService.cs
+++ Cirkla_Client/Services/ComponentNotificationService.cs
@@ -1,9 +1,9 @@
 ﻿namespace Cirkla_Client.Services
 {
     public class ComponentNotificationService
-    {            
+    {
         // Action to notify components of updates
-        public Action OnNotify { get; set; }
+        public Action? OnNotify;
 
         // Method to trigger the notification
         public void NotifyStateChanged() => OnNotify?.Invoke();
