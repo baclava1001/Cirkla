35167c15f56778a2c6fbe6290b39d49ddc33de52
Samsal <43518070+baclava1001@users.noreply.github.com>
2025-02-11 08:42:03 +0100

    Experimenting with timeline for borrowing process.

--- Cirkla_DAL/Models/Contract.cs
+++ Cirkla_DAL/Models/Contract.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using Cirkla_DAL.Models.Enums;
 
 
 namespace Cirkla_DAL.Models
@@ -17,14 +18,19 @@ namespace Cirkla_DAL.Models
         [Required]
         public User Borrower { get; set; }
         [Required]
+        public DateTime Created { get; set; }
+        [Required]
         public DateTime StartTime { get; set; }
         [Required]
         public DateTime EndTime { get; set; }
-        [Required]
-        public DateTime Created { get; set; }
         public DateTime? AcceptedByOwner { get; set; }
         public DateTime? DeniedByOwner { get; set; }
+        //public DateTime? CancelledByOwner { get; set; }
+        //public DateTime? AcceptedByBorrower { get; set; }
+        //public DateTime? CancelledByBorrower { get; set; }
+        //public DateTime? PickedUpTime { get; set; }
+        //public DateTime? ReturnedTime { get; set; }
+        //public ContractStatus Status { get; set; }
 
-        // TODO: Later add more timestamps for: AgreedStartTime, AgreedEndTime & ActualStartTime, ActualEndTime
     }
 }
