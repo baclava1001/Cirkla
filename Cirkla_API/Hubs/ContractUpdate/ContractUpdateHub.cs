6435a8c2ca9ca1739e744ffd644fe78bdf6fdbbc
Samsal <43518070+baclava1001@users.noreply.github.com>
2025-02-18 13:30:16 +0100

    Added a SignalR notificationhub for Contract updates

---
+++ Cirkla_API/Hubs/ContractUpdate/ContractUpdateHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using NuGet.Protocol.Providers;
+
+namespace Cirkla_API.Hubs.ContractUpdate;
+
+// TODO: Add authorization [Authorize] - check https://www.youtube.com/watch?v=O7oaxFgNuYo
+public class ContractUpdateHub : Hub<IContractUpdateHub>
+{
+    public override async Task OnConnectedAsync()
+    {
+        await Clients.Client(Context.ConnectionId).ReceiveContractUpdate($"{Context.ConnectionId} has joined the hub!");
+        await base.OnConnectedAsync();
+    }
+}
---
+++ Cirkla_API/Hubs/ContractUpdate/IContractUpdateHub.cs
@@ -0,0 +1,6 @@
+﻿namespace Cirkla_API.Hubs.ContractUpdate;
+
+public interface IContractUpdateHub
+{
+    Task ReceiveContractUpdate(string message);
+}
